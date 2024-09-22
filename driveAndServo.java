ac7bd62770dad530a7b5cc40c8de4dd2aebc447c
YusufLatif <yusuf.abdellatif@hotmail.com>
2024-09-22 13:37:29 -0500

    Update driveAndServo.java
    Used to move the vechile and to move one of the servos

--- driveAndServo.java
+++ driveAndServo.java
@@ -1,31 +1,4 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+
 
 package org.firstinspires.ftc.teamcode;
 
@@ -39,21 +12,6 @@ import com.qualcomm.robotcore.hardware.Servo;
  
 
 
-
-
-/*
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
 @TeleOp(name = "YA4", group = "LinearOpMode")
 
 public class YA4 extends LinearOpMode {
@@ -61,9 +19,8 @@ public class YA4 extends LinearOpMode {
     private DcMotor backleftDrive = null;
     private DcMotor frontrightDrive = null;
     private DcMotor backrightDrive = null;
-    Servo hookL;
-    Servo hookR;
-    Servo armServo = null;
+//    Servo hookR;
+    Servo hookL = null;
 
 
     @Override
@@ -73,7 +30,7 @@ public class YA4 extends LinearOpMode {
         backleftDrive = hardwareMap.get(DcMotor.class, "bl");
         backrightDrive = hardwareMap.get(DcMotor.class, "br");
         
-        armServo = hardwareMap.get(Servo.class, "leftHangerServo");
+        hookL = hardwareMap.get(Servo.class, "leftHangerServo");
         
         frontleftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontrightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -114,22 +71,22 @@ public class YA4 extends LinearOpMode {
             frontleftDrive.setPower(flpower);
             backleftDrive.setPower(blpower);
           
-         if (gamepad1.y){
+         // if (gamepad1.y){
               
-              hookL.setPosition(0);
+         //      hookL.setPosition(0);
               
-          } 
+         //  } 
           
-          else if(gamepad1.x){
-              hookL.setPosition(0.7);
+         //  else if(gamepad1.x){
+         //      hookL.setPosition(0.7);
               
-          }
+         //  }
           
-          else if(gamepad1.a){
+         //  else if(gamepad1.a){
               
-              hookL.setPosition(1);
+         //      hookL.setPosition(1);
               
-          }
+         //  }
 
          
             movearm();
@@ -144,17 +101,19 @@ public class YA4 extends LinearOpMode {
         
     
      public void movearm(){
+         telemetry.addData("moveArm", "Running");
         if (gamepad1.y ){
-            armServo.setPosition(0);
+            hookL.setPosition(0);
         }
         else if(gamepad1.b ){
-            armServo.setPosition(0.5);
+            hookL.setPosition(0.5);
         }
         else if(gamepad1.a){
-            armServo.setPosition(0.5);
+            hookL.setPosition(0.5);
         }
         else if(gamepad1.x){
-            armServo.setPosition(0);
+            hookL.setPosition(0);
         }
      }
+}
 }
